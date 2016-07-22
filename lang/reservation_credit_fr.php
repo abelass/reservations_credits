12382c59ab4c7455828b428361d353c3d0cceb8c
abelass <rainer@websimple.be>
2016-07-22 10:49:19 +0200

    debut paiement public crédit

--- lang/reservation_credit_fr.php
+++ lang/reservation_credit_fr.php
@@ -1,27 +1,26 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// This is a SPIP language file -- Ceci est un fichier langue de SPIP
+if (! defined ( '_ECRIRE_INC_VERSION' ))
+	return;
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
+$GLOBALS [$GLOBALS ['idx_lang']] = array(
+	
 	// A
 	'ajouter_lien_reservation_credit' => 'Ajouter ce crédit',
 	
 	//
-	'bouton_enregistrer_reglement_credit' => 'Régler avec un crédit',
-
+	'bouton_enregistrer_reglement_credit' => 'Régler avec mes crédits',
+	
 	// C
 	'champ_credit_label' => 'Crédit',
 	'champ_email_label' => 'Email',
-
+	
 	// I
 	'icone_creer_reservation_credit' => 'Créer un crédit',
 	'icone_modifier_reservation_credit' => 'Modifier ce crédit',
 	'info_1_reservation_credit' => 'Un crédit',
 	'info_aucun_reservation_credit' => 'Aucun crédit',
-	'info_credit_imprimer' => 'Vous avez ’Régler par chèque’.
+	'info_credit_imprimer' => 'Vous pouvez payez en utilisant vos Crédits qui s\'élèvent actuellement à @credit@ en cliquant us ’Régler avec mes credits’.
 
 Transaction : #@transaction@
 _ Montant : @montant@',
@@ -33,14 +32,14 @@ Les instructions concernant l’établissement du virement seront fournies aprè
 Transaction : #@transaction@
 _ Montant : @montant@
 ',
-
+	
 	// P
-	'payer_par_credit' => 'Payer avec crédit',
-
+	'payer_par_credit' => 'Payer avec mes crédits',
+	
 	// R
 	'retirer_lien_reservation_credit' => 'Retirer ce crédit',
 	'retirer_tous_liens_reservation_credits' => 'Retirer tous les crédits',
-
+	
 	// T
 	'texte_ajouter_reservation_credit' => 'Ajouter un crédit',
 	'texte_changer_statut_reservation_credit' => 'Ce crédit est :',
@@ -51,7 +50,7 @@ _ Montant : @montant@
 	'titre_logo_reservation_credit' => 'Logo de ce crédit',
 	'titre_reservation_credit' => 'Crédit',
 	'titre_reservation_credits' => 'Crédits',
-	'titre_reservation_credits_rubrique' => 'Crédits de la rubrique',
+	'titre_reservation_credits_rubrique' => 'Crédits de la rubrique' 
 );
 
 ?>
--- presta/credit/call/response.php
+++ presta/credit/call/response.php
@@ -1,28 +1,115 @@
 <?php
-/**
- * Stockage des transactionsde pipelines par Réservations Crédits
+/*
+ * Paiement Bancaire
+ * module de paiement bancaire multi prestataires
+ * stockage des transactions
  *
- * @plugin     Réseŕvations Crédits
- * @copyright  2015
- * @author     Rainer
- * @licence    GNU/GPL
- * @package    SPIP\Reservations_credits\Pipelines
+ * Auteurs :
+ * Cedric Morin, Nursit.com
+ * (c) 2012-2015 - Distribue sous licence GNU/GPL
+ *
  */
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
+if (! defined ( '_ECRIRE_INC_VERSION' ))
+	return;
 
 /**
- * Call response simple (cheque, virement)
  * il faut avoir un id_transaction et un transaction_hash coherents
  * pour se premunir d'une tentative d'appel exterieur
  *
- * @param array $config
- * @param null|array $response
+ * @param array $config        	
+ * @param null|array $response        	
  * @return array
  */
-function presta_credit_call_response_dist($config, $response=null){
-	include_spip('inc/bank');
-
-	return bank_simple_call_response($config, $response);
+function presta_gratuit_call_response_dist($config, $response = null) {
+	$mode = $config ['presta'];
+	// recuperer la reponse en post et la decoder, en verifiant la signature
+	if (! $response)
+		$response = bank_response_simple ( $mode );
+	
+	if (! isset ( $response ['id_transaction'] ) or ! isset ( $response ['transaction_hash'] )) {
+		return bank_transaction_invalide ( 0, array(
+			'mode' => $mode,
+			'erreur' => "id_transaction ou transaction_hash absent",
+			'log' => bank_shell_args ( $response ) 
+		) );
+	}
+	
+	$id_transaction = $response ['id_transaction'];
+	$transaction_hash = $response ['transaction_hash'];
+	
+	if (! $row = sql_fetsel ( '*', 'spip_transactions', 'id_transaction=' . intval ( $id_transaction ) )) {
+		return bank_transaction_invalide ( $id_transaction, array(
+			'mode' => $mode,
+			'erreur' => "transaction inconnue",
+			'log' => bank_shell_args ( $response ) 
+		) );
+	}
+	if ($transaction_hash != $row ['transaction_hash']) {
+		return bank_transaction_invalide ( $id_transaction, array(
+			'mode' => $mode,
+			'erreur' => "id_transaction $id_transaction, hash $transaction_hash non conforme",
+			'log' => bank_shell_args ( $response ) 
+		) );
+	}
+	
+	// verifier si le montant payé est égale ou inférieure au monant requis
+	
+	// Le cas normal d'une reservation
+	if ($id_reservation = $row ['id_reservation']) {
+		$sql = sql_select ( 'prix,prix_ht,taxe,quantite', 'spip_reservations_details', 'status!=' . sql_quote ( 'poubelle' ) . ' AND id_reservation=' . $id_reservation );
+		$prix = array();
+		while ( $row = sql_fetch ( $sql ) ) {
+			if (intval ( $row ['prix'] ) > 0 or floatval ( $row ['prix'] ) > 0.00) {
+				$prix [] = $row ['prix'] * $row ['quantite'];
+			}
+			else {
+				$prix [] = $row ['prix_ht'] * $row ['quantite'];
+			}
+			$prix_du = array_sum ( $prix );
+		}
+	}
+	elseif ($id_commande = $row ['id_commande']) {
+		$sql = sql_select ( 'prix,prix_ht,taxe,quantite', 'spip_reservations_details', 'status!=' . sql_quote ( 'poubelle' ) . ' AND id_reservation=' . $id_reservation );
+		$prix = array();
+		while ( $row = sql_fetch ( $sql ) ) {
+			if (intval ( $row ['prix'] ) > 0 or floatval ( $row ['prix'] ) > 0.00) {
+				$prix [] = $row ['prix'] * $row ['quantite'];
+			}
+			else {
+				$prix [] = $row ['prix_ht'] * $row ['quantite'];
+			}
+			$prix_du = array_sum ( $prix );
+		}
+	}
 }
+
+if (intval ( $row ['montant'] ) > 0 or floatval ( $row ['montant'] ) > 0.00) {
+	return bank_transaction_invalide ( $id_transaction, array(
+		'mode' => $mode,
+		'erreur' => "id_transaction $id_transaction, montant " . $row ['montant'] . ">0 interdit",
+		'log' => bank_shell_args ( $response ) 
+	) );
+}
+
+// OK, on peut accepter le reglement
+$set = array(
+	"mode" => $mode,
+	"montant_regle" => $row ['montant'],
+	"date_paiement" => date ( 'Y-m-d H:i:s' ),
+	"statut" => 'ok',
+	"reglee" => 'oui' 
+);
+sql_updateq ( "spip_transactions", $set, "id_transaction=" . intval ( $id_transaction ) );
+spip_log ( "call_resonse : id_transaction $id_transaction, reglee", $mode );
+
+$regler_transaction = charger_fonction ( 'regler_transaction', 'bank' );
+$regler_transaction ( $id_transaction, array(
+	'row_prec' => $row 
+) );
+
+return array(
+	$id_transaction,
+	true 
+);
+}
+
