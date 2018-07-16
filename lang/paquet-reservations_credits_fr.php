ceceab71393ee7030929412e6a2158fb53da1d27
abelass <rainer@websimple.be>
2018-07-16 11:38:55 +0200

    explication

--- lang/paquet-reservations_credits_fr.php
+++ lang/paquet-reservations_credits_fr.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
-	'reservations_credits_description' => 'Permet d’annuler des événements et de convertir d’éventuels montants payés en crédits, utilisables pour d’autres réservations',
+	'reservations_credits_description' => 'Permet d’annuler des événements et de convertir les montants payés en crédits, utilisables pour payer d’autres réservations',
 	'reservations_credits_nom' => 'Réservations Crédits',
 	'reservations_credits_slogan' => 'Gestion de crédits pour vos réservations'
 );
